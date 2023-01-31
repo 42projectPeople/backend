fea94c0fcdfd00a2b908b5cd0333122d98c7cb0c
myukang <myunghwan0421@gmail.com>
2023-01-31 19:23:16 +0900

    FEAT: add HashtagController to HashtagModule

--- moim_back/src/hashtag/hashtag.module.ts
+++ moim_back/src/hashtag/hashtag.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Hashtag } from 'src/entity/Hashtag.entity';
+import { HashtagController } from './hashtag.controller';
 import { HashtagService } from './hashtag.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Hashtag])],
+  controllers: [HashtagController],
   providers: [HashtagService],
 })
 export class HashtagModule {}
