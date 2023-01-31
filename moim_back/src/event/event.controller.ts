c884e3138ef263f3744b0c009bac5d78462c0ced
9utty <leegh4250@gmail.com>
2023-01-31 17:28:45 +0900

    feat: first event get 데코레이터

--- moim_back/src/event/event.controller.ts
+++ moim_back/src/event/event.controller.ts
@@ -1,6 +1,9 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
 
 @Controller('event')
 export class EventController {
     @Get('/:id')
+    async HelloEvent{
+        console.log('hello')
+    }
 }
