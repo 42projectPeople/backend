67cb39cb6afef192f9be63500f57fedce3c5ead3
myukang <myunghwan0421@gmail.com>
2023-01-31 19:05:44 +0900

    FEAT: add hashtag repository

--- moim_back/src/hashtag/hashtag.service.ts
+++ moim_back/src/hashtag/hashtag.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Hashtag } from 'src/entity/Hashtag.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class HashtagService {}
+export class HashtagService {
+  constructor(
+    @InjectRepository(Hashtag)
+    private hashtagRepository: Repository<Hashtag>,
+  ) {}
+}
