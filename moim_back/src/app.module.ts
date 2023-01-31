d8874180d8814ce757c65727b9e8ffef2128da42
myukang <myunghwan0421@gmail.com>
2023-01-31 14:22:40 +0900

    FEAT: apply local configuration to App.module

--- moim_back/src/app.module.ts
+++ moim_back/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import localConfiguration from './config/localConfiguration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.dev.local'],
+      load: [localConfiguration],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
