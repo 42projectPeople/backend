ae20f52dbad76702b322074b8ebb9735f316fe9f
myukang <myunghwan0421@gmail.com>
2023-01-31 14:31:09 +0900

    FEAT: apply entitys in TypeOrmModule

--- moim_back/src/app.module.ts
+++ moim_back/src/app.module.ts
@@ -3,12 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import localConfiguration from './config/localConfiguration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hashtag } from './entity/Hashtag.entity';
+import { Event } from './entity/Event.entity';
+import { Review } from './entity/Review.entity';
+import { User } from './entity/User.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.dev.local'],
       load: [localConfiguration],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: Number.parseInt(process.env.PORT) | 3306,
+      username: process.env.DBUSER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [Event, Hashtag, Review, User],
+      synchronize: true, //특정 조건하에서 모든 데이터를 삭제하는 것 같습니다. 프로덕션에서는 사용하지 않는게 좋습니다.
+      logging: true,
     }),
   ],
   controllers: [AppController],
