10bb9b7215d51d4da3f74e7eba0939f0feb0ebde
myukang <myunghwan0421@gmail.com>
2023-01-31 19:05:53 +0900

    FEAT: imports hashtag

--- moim_back/src/hashtag/hashtag.module.ts
+++ moim_back/src/hashtag/hashtag.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { HashtagService } from './hashtag.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hashtag } from 'src/entity/Hashtag.entity';
 import { HashtagService } from './hashtag.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Hashtag])],
   providers: [HashtagService],
 })
 export class HashtagModule {}
