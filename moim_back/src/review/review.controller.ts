a9cdf89e35bafcf31cca3bc8d6f2b9f37aa85219
myukang <myunghwan0421@gmail.com>
2023-02-01 16:41:58 +0900

    FEAT: remove default crud

--- moim_back/src/review/review.controller.ts
+++ moim_back/src/review/review.controller.ts
@@ -25,16 +25,6 @@ export class ReviewController {
     return this.reviewService.create(createReviewDto);
   }
 
-  @Get()
-  findAll() {
-    return this.reviewService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewService.findOne(+id);
-  }
-
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
     return this.reviewService.update(+id, updateReviewDto);
