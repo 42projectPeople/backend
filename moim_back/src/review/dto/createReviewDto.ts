db24d215a968b6269da681913ba41c28f90e0960
myukang <myunghwan0421@gmail.com>
2023-02-02 17:20:34 +0900

    feat: add createReviewDto

--- moim_back/src/review/dto/createReviewDto.ts
+++ moim_back/src/review/dto/createReviewDto.ts
@@ -1,3 +1,22 @@
-export default interface CreateReviewDto {
-  test: string;
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNumber, IsString } from 'class-validator'
+
+export default class CreateReviewDto {
+  @ApiProperty({
+    description: 'content 내용',
+  })
+  @IsString()
+  content: string
+
+  @ApiProperty({
+    description: '작성자 userId',
+  })
+  @IsNumber()
+  reviewerId: number
+
+  @ApiProperty({
+    description: '댓글이 적힌 이벤트의 ID',
+  })
+  @IsNumber()
+  eventId: number
 }
