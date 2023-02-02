181fa7fe5b9e6ac4763430f41588d151f810a06f
myukang <myunghwan0421@gmail.com>
2023-02-03 03:13:35 +0900

    feat(UpdateReviewDto): add updateReviewDto
    PickType으로 content만을 받습니다.

--- moim_back/src/review/dto/updateReviewDto.ts
+++ moim_back/src/review/dto/updateReviewDto.ts
@@ -1,3 +1,6 @@
+import { PartialType, PickType } from '@nestjs/swagger'
 import CreateReviewDto from './createReviewDto'
 
-export class UpdateReviewDto extends CreateReviewDto {}
+export class UpdateReviewDto extends PartialType(
+  PickType(CreateReviewDto, ['content'] as const)
+) {}
