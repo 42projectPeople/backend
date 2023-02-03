50d26d56b668ad46a18ebff97761f28396bc527c
myukang <myunghwan0421@gmail.com>
2023-02-03 15:39:31 +0900

    feat(remove service): add remove service

--- moim_back/src/review/review.controller.ts
+++ moim_back/src/review/review.controller.ts
@@ -12,6 +12,7 @@ import {
 import { ReviewService } from './review.service'
 import CreateReviewDto from './dto/createReviewDto'
 import { UpdateReviewDto } from './dto/updateReviewDto'
+import { DeleteReviewDto } from './dto/deleteReviewDto'
 
 @Controller('review')
 export class ReviewController {
@@ -49,11 +50,25 @@ export class ReviewController {
      * if (req.user.userId != updateReviewDto.reviewerId)
      *	throw new ForbiddenException('FOrbidden access')
      * */
-    return this.reviewService.update(+reviewId, updateReviewDto)
+    return await this.reviewService.update(+reviewId, updateReviewDto)
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewService.remove(+id)
+  @Delete(':reviewId')
+  //need session guard
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    })
+  )
+  async deleteReview(
+    @Param('reviewId') reviewId: string,
+    @Body() deleteReviewDto: DeleteReviewDto
+  ) {
+    /*
+     * if (req.user.userId != deleteReviewDto.reviewerId)
+     * 	throw new Forbidden~
+     * */
+    return await this.reviewService.remove(+reviewId)
   }
 }
