8705117429f174c907434746a9451b29c16480de
myukang <myunghwan0421@gmail.com>
2023-02-03 15:30:36 +0900

    feat(column): delete  unneeded column
    eventId do not needed
    reviewId is in param

--- moim_back/src/review/dto/deleteReviewDto.ts
+++ moim_back/src/review/dto/deleteReviewDto.ts
@@ -3,11 +3,5 @@ import { IsNumber } from 'class-validator'
 import CreateReviewDto from './createReviewDto'
 
 export class DeleteReviewDto extends PartialType(
-  PickType(CreateReviewDto, ['reviewerId', 'eventId'] as const)
-) {
-  @ApiProperty({
-    description: '삭제할 review의 ID',
-  })
-  @IsNumber()
-  reviewId: number
-}
+  PickType(CreateReviewDto, ['reviewerId'] as const)
+) {}
