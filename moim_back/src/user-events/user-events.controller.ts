8f2ba31b236b6f7e82cfa9f055b0b6ff5e966bfa
myukang <myunghwan0421@gmail.com>
2023-02-04 15:25:49 +0900

    comment(each controller): add comments

--- moim_back/src/user-events/user-events.controller.ts
+++ moim_back/src/user-events/user-events.controller.ts
@@ -6,23 +6,35 @@ import { CreateUserEventDto } from './dto/create-user-event.dto'
 export class UserEventsController {
   constructor(private readonly userEventsService: UserEventsService) {}
 
+  /*
+   * get events info that guestId is participate in
+   * */
   @Get('/:guestId/asGuest')
   getEventsAsGuest() {
     return this.userEventsService.findAll()
   }
 
+  /*
+   * get events info that hostId is hosted
+   * */
   @Get('/:hostId/asHost')
   getEventsAsHost() {
     return this.userEventsService.findAll()
   }
 
+  /*
+   * create new participate info
+   * */
   @Post('/')
   postEventsParticipant(@Body() createUserEventDto: CreateUserEventDto) {
     return createUserEventDto
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userEventsService.remove(+id)
+  /*
+   * delete current participate
+   * */
+  @Delete(':participateId')
+  remove(@Param('participateId') participateId: string) {
+    return this.userEventsService.remove(+participateId)
   }
 }
