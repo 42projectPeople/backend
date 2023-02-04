2423672fd7be67c41fb7515e0c4e6cf92e3a4058
9utty <leegh4250@gmail.com>
2023-02-04 23:00:02 +0900

    refactor: event controller 수정

--- moim_back/src/event/event.controller.ts
+++ moim_back/src/event/event.controller.ts
@@ -16,17 +16,11 @@ export class EventController {
   @Get('/:id')
   async FindOneEvent(@Param('id') id: number): Promise<Event> {
     const ret = await this.eventService.eventFindOneById(id)
-    console.log(`\n\n ret = ${ret}`)
     return ret
   }
   @Post('')
   async CreateEvent(@Body() body: CreateEventDto) {
     const ret = await this.eventService.eventCreate(body)
-    if (!ret)
-      return new BadRequestException(
-        '해시태그 또는, 호스트가 존재하지 않습니다.'
-      )
-    console.log(`\nCreate :${ret}`)
     return ret
   }
 }
