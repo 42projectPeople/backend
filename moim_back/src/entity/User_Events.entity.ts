e774094914ec8ece1899bbac977bcf446c13dbf1
myukang <myunghwan0421@gmail.com>
2023-02-04 22:10:03 +0900

    feat(add soft delete columns): add soft delete columns

--- moim_back/src/entity/User_Events.entity.ts
+++ moim_back/src/entity/User_Events.entity.ts
@@ -4,15 +4,42 @@ import {
   JoinColumn,
   Unique,
   PrimaryGeneratedColumn,
+  Column,
 } from 'typeorm'
 import { User } from './User.entity'
 import { Event } from './Event.entity'
 
 @Entity()
-@Unique('unique_UserEvents_user_eventId', ['userId', 'eventId'])
+@Unique('unique_UserEvents_userId_eventId_participatedAt', [
+  'userId',
+  'eventId',
+  'participatedAt',
+])
 export class User_Events {
   @PrimaryGeneratedColumn()
   participentId: number
+
+  @Column({
+    type: 'datetime',
+    precision: 6,
+    default: () => 'CURRENT_TIMESTAMP',
+    nullable: false,
+  })
+  participatedAt: Date
+
+  @Column({
+    type: 'datetime',
+    default: null,
+    nullable: true,
+  })
+  deletedAt: Date
+
+  @Column({
+    type: 'boolean',
+    default: false,
+    nullable: false,
+  })
+  isDeleted: boolean
 
   @ManyToOne(() => User, (user) => user.userId, { nullable: false })
   @JoinColumn({
