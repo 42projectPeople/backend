2d1026f0403e6327daa0490ef56cddd05e32ed97
9utty <leegh4250@gmail.com>
2023-02-06 00:21:10 +0900

    feat: event.delete.dto.ts 에 @ApiProperty 추가

--- moim_back/src/event/dto/event.delete.dto.ts
+++ moim_back/src/event/dto/event.delete.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNumber } from 'class-validator'
 
 export class eventDeleteDto {
+  @ApiProperty({
+    example: '1',
+    description: '삭제할 event아이디',
+  })
   @IsNumber()
   eventId: number
   @IsNumber()
