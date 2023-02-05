5b660e64ef240d4b4205195a3f4b0addcbfb37f0
myukang <myunghwan0421@gmail.com>
2023-02-05 21:37:39 +0900

    refactor(unique, columns):
    unique constraint // -participatedAt +isDeleted
    use CreateDateColumn, DeleteDateColumn

--- moim_back/src/entity/User_Events.entity.ts
+++ moim_back/src/entity/User_Events.entity.ts
@@ -4,40 +4,34 @@ import {
   JoinColumn,
   Unique,
   PrimaryGeneratedColumn,
+  DeleteDateColumn,
+  CreateDateColumn,
   Column,
 } from 'typeorm'
 import { User } from './User.entity'
 import { Event } from './Event.entity'
 
 @Entity()
-@Unique('unique_UserEvents_userId_eventId_participatedAt', [
+@Unique('unique_UserEvents_userId_eventId_deletedAt', [
   'userId',
   'eventId',
-  'participatedAt',
+  'isDeleted',
 ])
 export class User_Events {
   @PrimaryGeneratedColumn()
   participentId: number
 
-  @Column({
-    type: 'datetime',
-    precision: 6,
-    default: () => 'CURRENT_TIMESTAMP',
-    nullable: false,
-  })
+  /*
+   * special columns for insertion date
+   * */
+  @CreateDateColumn()
   participatedAt: Date
 
-  @Column({
-    type: 'datetime',
-    default: null,
-    nullable: true,
-  })
-  deletedAt: string
+  @DeleteDateColumn()
+  deletedAt: Date
 
   @Column({
-    type: 'boolean',
     default: false,
-    nullable: false,
   })
   isDeleted: boolean
 
