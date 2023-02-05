edf909b7529c479c0f11ed2ccffa6ebac18b0820
myukang <myunghwan0421@gmail.com>
2023-02-05 21:35:05 +0900

    refactor(remove updatedto): commit

--- moim_back/src/user-events/dto/update-user-event.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserEventDto } from './create-user-event.dto';
-
-export class UpdateUserEventDto extends PartialType(CreateUserEventDto) {}
