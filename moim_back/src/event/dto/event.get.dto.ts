528d4f92bf7013c9b971a84fab6713861c3e0267
9utty <leegh4250@gmail.com>
2023-02-05 22:51:01 +0900

    refactor: event.get.dto.ts
    수정

--- moim_back/src/event/dto/event.get.dto.ts
+++ moim_back/src/event/dto/event.get.dto.ts
@@ -1,8 +1,18 @@
+import { ApiProduces, ApiProperty } from '@nestjs/swagger'
 import { IsNumber } from 'class-validator'
 
 export class EventGetDto {
+  @ApiProperty({
+    example: '1',
+    description: 'event고유 id',
+  })
   @IsNumber()
   eventId: number
+
+  @ApiProperty({
+    example: '1',
+    description: 'event를 생성한 User의 Id',
+  })
   @IsNumber()
   hostId: number
 }
