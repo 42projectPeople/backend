23dc91d58808310f6d50cd9171c0371b506000ec
Photogrammer <khsj15@khu.ac.kr>
2023-02-06 17:43:19 +0900

    chore : DTO 에 api property 추가

--- moim_back/src/user/dto/createUserDto.ts
+++ moim_back/src/user/dto/createUserDto.ts
@@ -1,25 +1,53 @@
-import { IsByteLength, IsNumber, IsString, IsUrl } from 'class-validator';
+import { IsByteLength, IsNumber, IsString, IsUrl } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+import { User } from '../../entity/User.entity'
 
 export class CreateUserDto {
-  @IsNumber()
-  userId: number;
-
+  @ApiProperty({
+    description: 'user의 고유한 name',
+  })
   @IsString()
   @IsByteLength(2, 35)
-  userName: string;
+  userName: string
 
+  @ApiProperty({
+    description: 'user nickname',
+  })
   @IsString()
   @IsByteLength(2, 35)
-  userNickName: string;
+  userNickName: string
 
+  @ApiProperty({
+    description: 'user 프로필 사진 url',
+  })
   @IsUrl()
   @IsByteLength(10, 100)
-  userProfilePhoto: string;
+  userProfilePhoto: string
 
+  @ApiProperty({
+    description: 'user level',
+  })
   @IsNumber()
-  userLevel: number;
+  userLevel: number
 
+  @ApiProperty({
+    description: 'user 가 자신을 소개하는 짧은 글',
+  })
   @IsString()
   @IsByteLength(0, 200)
-  userTitle: string;
+  userTitle: string
+
+  /**
+   * DTO to entity
+   * @param createReviewDto
+   */
+  static toEntity(createReviewDto: CreateUserDto): User {
+    const user = new User()
+    user.userName = createReviewDto.userName
+    user.userNickName = createReviewDto.userNickName
+    user.userTitle = createReviewDto.userTitle
+    user.userProfilePhoto = createReviewDto.userProfilePhoto
+    user.userLevel = createReviewDto.userLevel
+    return user
+  }
 }
