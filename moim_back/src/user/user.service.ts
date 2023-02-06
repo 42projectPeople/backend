4efd53a1847e724324252dccb686a1fb1e9428f2
Photogrammer <khsj15@khu.ac.kr>
2023-02-06 17:44:32 +0900

    chore : user service 에서 typeorm 을 통한 레포지토리 접근 구현

--- moim_back/src/user/user.service.ts
+++ moim_back/src/user/user.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from '../entity/User.entity'
+import { Repository } from 'typeorm'
+import { UpdateUserDto } from './dto/updateUserDto'
+import { CreateUserDto } from './dto/createUserDto'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>
+  ) {}
+
+  async find(userId: number) {
+    return await this.userRepository.find({
+      where: { userId: userId },
+    })
+  }
+
+  async update(userId: number, userInfo: UpdateUserDto) {
+    try {
+      await this.userRepository
+        .createQueryBuilder()
+        .update()
+        .set({
+          userName: userInfo.userName,
+          userNickName: userInfo.userNickName,
+          userTitle: userInfo.userTitle,
+          userProfilePhoto: userInfo.userProfilePhoto,
+        })
+        .where('userId = :id', { id: userId })
+        .execute()
+    } catch (err) {
+      throw new InternalServerErrorException('database server error')
+    }
+  }
+
+  async create(userInfo: CreateUserDto) {
+    try {
+      await this.userRepository
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values(CreateUserDto.toEntity(userInfo))
+        .execute()
+    } catch (err) {
+      throw new InternalServerErrorException('database server error')
+    }
+  }
+}
