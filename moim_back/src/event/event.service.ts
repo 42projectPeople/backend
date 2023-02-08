c2cef24e86eddf0199fcfded194bc736bde93df0
9utty <leegh4250@gmail.com>
2023-02-08 15:22:17 +0900

    feat: event.retrun.dto 수정

--- moim_back/src/event/event.service.ts
+++ moim_back/src/event/event.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { env } from 'process'
 import { NotFoundError } from 'rxjs'
 import { Event } from 'src/entity/Event.entity'
 import { Hashtag } from 'src/entity/Hashtag.entity'
 import { User } from 'src/entity/User.entity'
 import { Repository } from 'typeorm'
 import { EventCreateDto } from './dto/event.create.dto'
+import { EventReturnDto } from './dto/event.return.dto'
 import { EventUpdateDto } from './dto/event.update.dto'
 
 @Injectable()
@@ -20,33 +22,65 @@ export class EventService {
     return await this.eventRepository.findOneBy({ eventId })
   }
 
-  async eventCreate(event: EventCreateDto): Promise<any> {
-    const tmp_event = new Event()
-
-    tmp_event.eventDate = event.eventDate
-    tmp_event.main_image = event.main_image
-    tmp_event.content = event.content
-    tmp_event.location = event.location
-    tmp_event.latitude = event.latitude
-    tmp_event.longitude = event.longitude
-    tmp_event.header = event.header
-    tmp_event.hashtag = event.hashtag
-    tmp_event.host = event.host
-    tmp_event.maxParticipant = event.maxParticipant
-    tmp_event.curParticipant = 0
-    tmp_event.rating = 0
-
+  async eventCreate(newEvent: EventCreateDto): Promise<EventReturnDto> {
     try {
-      const ret = await this.eventRepository
+      const createEvent = this.transCreateDto(newEvent)
+      const event = await this.eventRepository.save(createEvent)
+      const user = await this.userRepository
         .createQueryBuilder()
-        .insert()
-        .into(Event)
-        .values(tmp_event)
-        .execute()
-      return ret
+        .innerJoin('user', 'user')
+        .where('user.userId=:userId', { userId: event.host })
+        .getOne()
+      const hashtag = await this.hashtagRepository
+        .createQueryBuilder()
+        .innerJoin('hashtag', 'h')
+        .where('h.hashtagId=:hashtag', { hashtag: event.hashtag })
+        .getOne()
+      const returnDto = await this.transReturnDto(user, event, hashtag)
+
+      return returnDto
     } catch (e) {
       throw new NotFoundException('db injection')
     }
+  }
+
+  transCreateDto(newEvent: EventCreateDto) {
+    const event = new Event()
+
+    event.eventDate = newEvent.eventDate
+    event.main_image = newEvent.main_image
+    event.content = newEvent.content
+    event.location = newEvent.location
+    event.latitude = newEvent.latitude
+    event.longitude = newEvent.longitude
+    event.header = newEvent.header
+    event.hashtag = newEvent.hashtag
+    event.host = newEvent.host
+    event.maxParticipant = newEvent.maxParticipant
+    event.curParticipant = 0
+    event.rating = 0
+
+    return event
+  }
+
+  transReturnDto(user: User, event: Event, hashtag: Hashtag) {
+    const returnDto = new EventReturnDto()
+
+    returnDto.userNickName = user.userNickName
+    returnDto.userProfilePhoto = user.userProfilePhoto
+    returnDto.userTitle = user.userTitle
+    returnDto.hashtagName = hashtag.hashtagName
+    returnDto.main_image = event.main_image
+    returnDto.header = event.header
+    returnDto.content = event.content
+    returnDto.views = event.views
+    returnDto.location = event.location
+    returnDto.latitude = event.latitude
+    returnDto.longitude = event.longitude
+    returnDto.maxParticipant = event.maxParticipant
+    returnDto.curParticipant = event.curParticipant
+
+    return returnDto
   }
 
   async eventUpdate(id: number, update: EventUpdateDto) {
@@ -67,6 +101,7 @@ export class EventService {
       })
       .where('eventId = :id', { id: id })
       .execute()
+
     return event
   }
 }
