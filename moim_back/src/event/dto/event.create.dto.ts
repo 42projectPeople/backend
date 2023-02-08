364ec30e1c0a4d024c90e2dd2ea6ae1a8061d4c5
9utty <leegh4250@gmail.com>
2023-02-08 23:30:04 +0900

    feat: event @Get 데코레이터 수정

--- moim_back/src/event/dto/event.create.dto.ts
+++ moim_back/src/event/dto/event.create.dto.ts
@@ -1,15 +1,34 @@
 import { PickType } from '@nestjs/swagger'
 import { Event } from 'src/entity/Event.entity'
 
-export class EventCreateDto extends PickType(Event, [
-  'eventDate',
-  'main_image',
-  'content',
-  'location',
-  'latitude',
-  'longitude',
-  'header',
-  'maxParticipant',
-  'hashtag',
-  'host',
-]) {}
+export class EventCreateDto {
+  eventDate: string
+  main_image: string
+  content: string
+  location: string
+  latitude: number
+  longitude: number
+  header: string
+  maxParticipant: number
+  hashtag: number
+  host: number
+
+  static transCreateDto(newEvent: EventCreateDto) {
+    const event = new Event()
+
+    event.eventDate = newEvent.eventDate
+    event.main_image = newEvent.main_image
+    event.content = newEvent.content
+    event.location = newEvent.location
+    event.latitude = newEvent.latitude
+    event.longitude = newEvent.longitude
+    event.header = newEvent.header
+    event.hashtag = newEvent.hashtag
+    event.host = newEvent.host
+    event.maxParticipant = newEvent.maxParticipant
+    event.curParticipant = 0
+    event.rating = 0
+
+    return event
+  }
+}
