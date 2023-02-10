20af0423630809b18793a21fce5cff4ace1b8b5c
myukang <myunghwan0421@gmail.com>
2023-02-11 02:20:29 +0900

    feat(check): add controller @Get /health

--- moim_back/src/health/health.controller.ts
+++ moim_back/src/health/health.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common'
-import { Response } from 'express'
+import { Controller, Get } from '@nestjs/common'
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus'
 
 @Controller('health')
 export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator
+  ) {}
+
   /*
    * @param response object
    * default healthcheck controller for LOADBALANCER
    * */
   @Get()
-  getHealth(@Res() res: Response) {
-    res.status(HttpStatus.OK).json([])
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.db.pingCheck('database'), //백엔드 서버 데이터베이스의 상태를 확인합니다.
+      //redis session database check line----
+    ])
   }
 }
