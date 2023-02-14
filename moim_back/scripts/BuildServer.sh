de83c0294b866a85ace2d0426cf4feb03b584da4
koreanddinghwan <myunghwan0421@gmail.com>
2023-02-15 00:20:01 +0900

    feat(add scripts): commit

---
+++ moim_back/scripts/BuildServer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Nest.js 서버 빌드 스크립트
+
+#실행인자
+NPM=npm
+NPM_ARG=run
+BUILD_OPTION=build
+
+#change dir
+cd moim_back
+
+#build
+$NPM $NPM_ARG $BUILD_OPTION
+
+
+
---
+++ moim_back/scripts/StartServer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Nest.js 서버 시작 스크립트
+
+#실행인자
+NPM=npm
+NPM_ARG=run
+PROD_OPTION=start:prod
+
+#change dir
+cd moim_back
+
+# Nest.js 서버 실행
+$NPM $NPM_ARG $PROD_OPTION
