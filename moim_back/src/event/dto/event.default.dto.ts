12f1ce6b70e140620ad6655e2564b3a5303e4da6
9utty <leegh4250@gmail.com>
2023-02-17 15:59:34 +0900

    feat: eventDefault.dto.ts class-validate 추가

--- moim_back/src/event/dto/event.default.dto.ts
+++ moim_back/src/event/dto/event.default.dto.ts
@@ -1,15 +1,26 @@
+import { IsISBN, IsNumber, IsString } from 'class-validator'
 import { Event } from 'src/entity/Event.entity'
 
 export class EventDefaultDto {
+  @IsString()
   eventDate: string
+  @IsString()
   main_image: string
+  @IsString()
   content: string
+  @IsString()
   location: string
+  @IsNumber()
   latitude: number
+  @IsNumber()
   longitude: number
+  @IsString()
   header: string
+  @IsNumber()
   maxParticipant: number
+  @IsNumber()
   hashtag: number
+  @IsNumber()
   host: number
 
   static transEventDto(newEvent: EventDefaultDto) {
