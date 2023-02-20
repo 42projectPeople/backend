599add2a93a2664182701ef45330a23acf307e05
myukang <myunghwan0421@gmail.com>
2023-02-21 03:36:17 +0900

    feat(add pageSize): commit

--- moim_back/src/hashtag/dto/controllerEventByHashtagId.dto.ts
+++ moim_back/src/hashtag/dto/controllerEventByHashtagId.dto.ts
@@ -1,11 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { Type } from 'class-transformer'
-import { IsBoolean, IsNumber } from 'class-validator'
+import { Transform, Type } from 'class-transformer'
+import { IsBoolean, IsNumber, IsOptional } from 'class-validator'
 
-export abstract class controllerEventByHashtagIdDto {
+export class controllerEventByHashtagIdDto {
   @ApiProperty({
     description: '요청할 페이지',
     required: true,
+    example:
+      'https://damoimapp.com/hashtag/events/2?page=10&recommendation=true',
   })
   @Type(() => Number)
   @IsNumber()
@@ -16,12 +18,22 @@ export abstract class controllerEventByHashtagIdDto {
     required: false,
     default: false,
   })
-  @Type(() => Boolean)
-  @IsBoolean()
+  @IsOptional()
+  @IsBoolean() //https://github.com/typestack/class-transformer/issues/626#issuecomment-1125527201
+  @Transform(({ value }) => {
+    if (value === 'true') return true
+    if (value === 'false') return false
+  })
   readonly recommendation?: boolean
 
-  constructor(page: number, rec: boolean) {
-    this.page = page
-    this.recommendation = rec ?? false
-  }
+  @ApiProperty({
+    description: 'page 크기',
+    required: false,
+    default: 10,
+    example: 'https://damoimapp.com/hashtag/events/2?page=10&pageSize=2',
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  readonly pageSize?: number
 }
