52d0f83023c355957b5d2f5e165f0a34e574e23a
myukang <myunghwan0421@gmail.com>
2023-02-24 07:19:06 +0900

    docs(): commit

--- moim_back/src/review/swagger/DocsGetReviewByUserId.docs.ts
+++ moim_back/src/review/swagger/DocsGetReviewByUserId.docs.ts
@@ -1,10 +1,5 @@
 import { applyDecorators } from '@nestjs/common'
-import {
-  ApiOperation,
-  ApiOkResponse,
-  ApiParam,
-  ApiQuery,
-} from '@nestjs/swagger'
+import { ApiOperation, ApiOkResponse } from '@nestjs/swagger'
 import ReturnReviews from '../dto/ReturnReview.dto'
 
 export function DocsGetReviewByUserId() {
@@ -16,12 +11,6 @@ export function DocsGetReviewByUserId() {
     ApiOkResponse({
       description: 'Get Success',
       type: ReturnReviews,
-    }),
-    ApiParam({
-      name: '작성한 리뷰를 요청할 사용자 아이디',
-    }),
-    ApiQuery({
-      name: 'pagination 정보',
     })
   )
 }
