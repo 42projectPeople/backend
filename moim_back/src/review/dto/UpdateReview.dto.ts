f84ba757ae2f94da4cb2ef9bfb2ad4c66f37969d
myukang <myunghwan0421@gmail.com>
2023-02-24 05:13:14 +0900

    feat(update dto): commit

---
+++ moim_back/src/review/dto/UpdateReview.dto.ts
@@ -0,0 +1,9 @@
+import { PickType } from '@nestjs/swagger'
+import CreateReviewDto from './createReview.dto'
+/*
+ * used in controller layer
+ * exposed in api
+ * */
+export class UpdateReviewDto extends PickType(CreateReviewDto, [
+  'content',
+] as const) {}
