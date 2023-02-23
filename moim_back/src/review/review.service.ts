fe657129beb8040f73486e404aa296e9fc27626f
myukang <myunghwan0421@gmail.com>
2023-02-24 07:19:19 +0900

    feat(controller): commit

--- moim_back/src/review/review.service.ts
+++ moim_back/src/review/review.service.ts
@@ -7,9 +7,12 @@ import {
 import { InjectRepository } from '@nestjs/typeorm'
 import { Review } from 'src/entity/Review.entity'
 import { DataSource, Repository } from 'typeorm'
+import { UpdateReviewDto } from './dto/UpdateReview.Dto'
 import CreateReviewDto from './dto/createReview.dto'
-import { ServiceDeleteReviewDto } from './dto/serviceDeleteReview.dto'
-import { ServiceUpdateReviewDto } from './dto/serviceUpdateReview.dto'
+import {
+  ServiceGetReviewByEventId,
+  ServiceGetReviewByUserId,
+} from './dto/ServiceGetReview.dto'
 
 @Injectable()
 export class ReviewService {
@@ -19,21 +22,64 @@ export class ReviewService {
     private datasource: DataSource
   ) {}
 
-  async findReviewByEventID(eventId: number): Promise<Review[]> {
+  async findReviewByEventID(serviceDto: ServiceGetReviewByEventId) {
     try {
-      return await this.reviewRepository.find({
-        where: { eventId: eventId, isDeleted: 'N' },
-      })
+      const qb = this.reviewRepository
+        .createQueryBuilder('r')
+        .innerJoin('r.eventId', 'e', 'e.eventId = r.eventId')
+        .innerJoinAndSelect('r.reviewerId', 'u', 'u.userId = r.reviewerId')
+        .select([
+          'r.reviewId',
+          'r.createdAt',
+          'r.modifiedAt',
+          'r.likes',
+          'r.content',
+          'r.reviewerId',
+          'u.userId',
+          'u.userName',
+          'u.userNickName',
+          'u.userProfilePhoto',
+          'u.userLevel',
+        ])
+        .where("r.eventId = :id AND r.isDeleted = 'N'", {
+          id: serviceDto.getEventId(),
+        })
+        .offset(serviceDto.getStartIndex())
+        .limit(serviceDto.getPageSize())
+      return await qb.getMany()
     } catch (e) {
+      console.log(e.message)
       throw new InternalServerErrorException()
     }
   }
 
-  async findReviewByUserId(userId: number): Promise<Review[]> {
+  async findReviewByUserId(
+    serviceDto: ServiceGetReviewByUserId
+  ): Promise<Review[]> {
     try {
-      return await this.reviewRepository.find({
-        where: { reviewerId: userId, isDeleted: 'N' },
-      })
+      const qb = this.reviewRepository
+        .createQueryBuilder('r')
+        .innerJoin('r.eventId', 'e', 'e.eventId = r.eventId')
+        .innerJoinAndSelect('r.reviewerId', 'u', 'u.userId = r.reviewerId')
+        .select([
+          'r.reviewId',
+          'r.createdAt',
+          'r.modifiedAt',
+          'r.likes',
+          'r.content',
+          'r.reviewerId',
+          'u.userId',
+          'u.userName',
+          'u.userNickName',
+          'u.userProfilePhoto',
+          'u.userLevel',
+        ])
+        .where("reviewerId = :id AND isDeleted = 'N'", {
+          id: serviceDto.getUserId(),
+        })
+        .offset(serviceDto.getStartIndex())
+        .limit(serviceDto.getPageSize())
+      return await qb.getMany()
     } catch (e) {
       throw new InternalServerErrorException()
     }
@@ -59,18 +105,23 @@ export class ReviewService {
    * udpate review comment
    * @param updateReviewDto
    * */
-  async update(serviceUpdateReviewDto: ServiceUpdateReviewDto): Promise<void> {
+  async update(
+    updateReviewDto: UpdateReviewDto,
+    reviewId: number,
+    userId: number
+  ): Promise<void> {
     const queryRunner = this.datasource.createQueryRunner()
     try {
       await queryRunner.connect() //connection pool에서 connection을 가져옵니다.
       await queryRunner.startTransaction()
+      //내용 업데이트
       const queryResult = await queryRunner.manager.update(
         Review, //업데이트할 테이블
         {
-          reviewId: serviceUpdateReviewDto.getReviewId(),
-          reviewerId: serviceUpdateReviewDto.getUserId(),
+          reviewId: reviewId,
+          reviewerId: userId,
         }, //조건
-        serviceUpdateReviewDto.getPartial()
+        { content: updateReviewDto.getContent() }
         //업데이트할 partial 객체
       )
 
@@ -87,7 +138,11 @@ export class ReviewService {
     }
   }
 
-  async remove(serviceDeleteReviewDto: ServiceDeleteReviewDto): Promise<void> {
+  /*
+   * @param reviewId: from url
+   * @param userId: from jwt token
+   * */
+  async remove(reviewId: number, userId: number): Promise<void> {
     const queryRunner = this.datasource.createQueryRunner()
     try {
       await queryRunner.connect() //connection pool에서 connection을 가져옵니다.
@@ -96,8 +151,8 @@ export class ReviewService {
       const queryResult = await queryRunner.manager.update(
         Review,
         {
-          reviewId: serviceDeleteReviewDto.getReviewId(),
-          reviewerId: serviceDeleteReviewDto.getUserId(),
+          reviewId: reviewId,
+          reviewerId: userId,
         },
         { isDeleted: 'Y', deletedAt: 'Date.now()' } //isDeleted
       )
@@ -108,6 +163,8 @@ export class ReviewService {
       await queryRunner.commitTransaction() //성공시 commit
     } catch (err) {
       await queryRunner.rollbackTransaction() //실패시 rollback
+      if (err.status === 404) throw new NotFoundException()
+      if (err.status === 500) throw new InternalServerErrorException()
     } finally {
       await queryRunner.release() //connection pool에 반환
     }
