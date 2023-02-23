1c1b97204e7daf64f68c2ff2e74f60bbe104def5
myukang <myunghwan0421@gmail.com>
2023-02-24 05:18:09 +0900

    docs(examples, required): commit

--- moim_back/src/review/dto/createReview.dto.ts
+++ moim_back/src/review/dto/createReview.dto.ts
@@ -5,18 +5,24 @@ import { Review } from 'src/entity/Review.entity'
 export default class CreateReviewDto {
   @ApiProperty({
     description: 'content 내용',
+    required: true,
+    example: 'hello! salut!',
   })
   @IsString()
   readonly content: string
 
   @ApiProperty({
     description: '작성자 userId',
+    required: true,
+    example: 1,
   })
   @IsNumber()
   readonly reviewerId: number
 
   @ApiProperty({
     description: '댓글이 적힌 이벤트의 ID',
+    required: true,
+    example: 56,
   })
   @IsNumber()
   readonly eventId: number
