601c4a3d344e74892c45776cf54b1156b04e9c05
myukang <myunghwan0421@gmail.com>
2023-03-05 04:34:02 +0900

    feat(hashtag):
    documentation 정리
    return dto 삭제

---
+++ moim_back/src/hashtag/swagger/DocsGetEventsByHashtag.dto.ts
@@ -0,0 +1,33 @@
+import { applyDecorators } from '@nestjs/common'
+import {
+  ApiOperation,
+  ApiOkResponse,
+  ApiInternalServerErrorResponse,
+  ApiParam,
+} from '@nestjs/swagger'
+import { Event } from 'src/entity/Event.entity'
+
+export function DocsGetEventsByHashtag() {
+  return (
+    applyDecorators(
+      ApiOkResponse({
+        description: '해시태그 ID를 가진 모든 이벤트를 반환합니다.',
+        type: Event,
+      }),
+      ApiOperation({
+        summary: 'hashtagId를 가진 이벤트를 반환합니다.',
+        description: 'hashtagId를 가진 이벤트를 반환합니다.',
+      }),
+      ApiInternalServerErrorResponse({
+        description: 'internal server error',
+      })
+    ),
+    ApiParam({
+      name: 'hashtagId',
+      description: '이벤트가 가진 해시태그의 아이디',
+      required: true,
+      type: Number,
+      example: 1,
+    })
+  )
+}
