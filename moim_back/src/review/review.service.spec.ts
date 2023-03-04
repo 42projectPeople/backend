058484faf96be68625feda81544e37176a86b25c
myukang <myunghwan0421@gmail.com>
2023-03-05 02:09:38 +0900

    prettier(세미콜론적용): commit

--- moim_back/src/review/review.service.spec.ts
+++ moim_back/src/review/review.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ReviewService } from './review.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ReviewService } from './review.service'
 
 describe('ReviewService', () => {
-  let service: ReviewService;
+  let service: ReviewService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ReviewService],
-    }).compile();
+    }).compile()
 
-    service = module.get<ReviewService>(ReviewService);
-  });
+    service = module.get<ReviewService>(ReviewService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
