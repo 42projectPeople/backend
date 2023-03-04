22216dc8c195ff03e8351f5a8c9f3944859d934c
myukang <myunghwan0421@gmail.com>
2023-03-05 02:07:16 +0900

    docs(return dto): review entity example추가해서 해결

--- moim_back/src/entity/Review.entity.ts
+++ moim_back/src/entity/Review.entity.ts
@@ -1,4 +1,4 @@
-import { ApiTags } from '@nestjs/swagger'
+import { ApiProperty, ApiTags } from '@nestjs/swagger'
 import {
   Entity,
   Column,
@@ -17,18 +17,38 @@ import { User } from './User.entity'
 @ApiTags('review api')
 @Unique('unique_Review_createdAt_reviewerId', ['createdAt', 'reviewerId'])
 export class Review {
+  @ApiProperty({
+    description: '리뷰의 아이디',
+    example: 1,
+  })
   @PrimaryGeneratedColumn({ primaryKeyConstraintName: 'pk_Review' })
   reviewId: number
 
+  @ApiProperty({
+    description: '생성날짜',
+    example: '2023-02-03 03:29:22.350000',
+  })
   @CreateDateColumn()
   createdAt: Date
 
+  @ApiProperty({
+    description: '수정날짜',
+    example: '2023-02-11 19:37:41.845766',
+  })
   @UpdateDateColumn()
   modifiedAt: Date
 
+  @ApiProperty({
+    description: '삭제날짜',
+    example: '2023-02-14 19:37:41.845766',
+  })
   @DeleteDateColumn()
   deletedAt: Date
 
+  @ApiProperty({
+    description: '삭제 유무',
+    example: 'N',
+  })
   @Column({
     type: 'enum',
     enum: ['Y', 'N'],
@@ -36,12 +56,20 @@ export class Review {
   })
   isDeleted: 'Y' | 'N'
 
+  @ApiProperty({
+    description: '좋아요 수',
+    example: '5',
+  })
   @Column({
     type: 'int',
     default: 0,
   })
   likes: number
 
+  @ApiProperty({
+    description: '리뷰 내용',
+    example: '좋아용 너무 재밌어용',
+  })
   @Column({
     type: 'varchar',
     length: 400,
