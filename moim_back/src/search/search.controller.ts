08f0f88e9de9339ae58520ed8ec04b0d97b82e07
myukang <myunghwan0421@gmail.com>
2023-03-05 00:46:09 +0900

    feat(controller):
    1. init controller
    2. apply docs decorator
    3. apply query dto

---
+++ moim_back/src/search/search.controller.ts
@@ -0,0 +1,64 @@
+import {
+  Controller,
+  Get,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { EventSearchDto } from './dto/EventSearch.dto'
+import { HashtagSearchDto } from './dto/HashtagSearch.dto'
+import { UserSearchDto } from './dto/UserSearch.dto'
+import { SearchService } from './search.service'
+import { DocsGetSearchEvent } from './swagger/DocsGetSearchEvent.dto'
+import { DocsGetSearchHashtag } from './swagger/DocsGetSearchHashtag.dto'
+import { DocsGetSearchUser } from './swagger/DocsGetSearchUser.dto'
+
+@Controller('search')
+@ApiTags('search api')
+export class SearchController {
+  constructor(private readonly searchService: SearchService) {}
+
+  @DocsGetSearchUser()
+  @Get('/user')
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    })
+  )
+  async getSearchUser(@Query() userSearchDto: UserSearchDto) {
+    return userSearchDto
+    return await this.searchService.searchUser()
+  }
+
+  @DocsGetSearchEvent()
+  @Get('/event')
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    })
+  )
+  async getSearchEvent(@Query() eventSearchDto: EventSearchDto) {
+    return await this.searchService.searchEvent()
+  }
+
+  @DocsGetSearchHashtag()
+  @Get('/hashtag')
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    })
+  )
+  async getSearchHashtag(@Query() hashtagDto: HashtagSearchDto) {
+    return await this.searchService.searchHashtag()
+  }
+}
