dc0ad5494f4508f2399d436ebb2a12ba793466d9
myukang <myunghwan0421@gmail.com>
2023-03-05 14:57:08 +0900

    docs(사용자이름=>닉네임)

--- moim_back/src/search/swagger/DocsGetSearchUser.dto.ts
+++ moim_back/src/search/swagger/DocsGetSearchUser.dto.ts
@@ -9,8 +9,8 @@ import { User } from 'src/entity/User.entity'
 export function DocsGetSearchUser() {
   return applyDecorators(
     ApiOperation({
-      summary: '사용자 이름으로 검색합니다.',
-      description: '사용자 이름으로 검색합니다.',
+      summary: '사용자 닉네임으로 검색합니다.',
+      description: '사용자 닉네임으로 검색합니다.',
     }),
     ApiOkResponse({
       description: '성공적으로 찾았습니다.',
@@ -18,7 +18,7 @@ export function DocsGetSearchUser() {
       type: User,
     }),
     ApiNoContentResponse({
-      description: '해당 사용자가 없습니다.',
+      description: '해당 단어를 닉네임에 가지는 사용자가 없습니다.',
       isArray: true,
     })
   )
