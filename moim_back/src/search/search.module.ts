42ac17d28ba60a92cef71e5fb0896c2a19ef8a57
myukang <myunghwan0421@gmail.com>
2023-03-05 16:27:00 +0900

    fix(Event)
    전역객체 Event가져오던 오류 수정

--- moim_back/src/search/search.module.ts
+++ moim_back/src/search/search.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Hashtag } from 'src/entity/Hashtag.entity'
 import { User } from 'src/entity/User.entity'
+import { Event } from 'src/entity/Event.entity'
 import { SearchController } from './search.controller'
 import { SearchService } from './search.service'
 
