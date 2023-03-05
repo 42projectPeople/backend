5299127e68ef19f07529c92c85126d2daed4ef7d
myukang <myunghwan0421@gmail.com>
2023-03-05 15:00:18 +0900

    feat(imports, InjectRepository): commit

--- moim_back/src/search/search.service.ts
+++ moim_back/src/search/search.service.ts
@@ -1,21 +1,24 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Hashtag } from 'src/entity/Hashtag.entity'
 import { User } from 'src/entity/User.entity'
 import { Event } from 'src/entity/Event.entity'
 import { Repository } from 'typeorm'
+import { UserSearchDto } from './dto/UserSearch.dto'
+import { EventSearchDto } from './dto/EventSearch.dto'
+import { HashtagSearchDto } from './dto/HashtagSearch.dto'
 
 @Injectable()
 export class SearchService {
   constructor(
-    @InjectRepository(Hashtag)
-    private hashtagRepo: Repository<Hashtag>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
 
     @InjectRepository(Event)
     private eventRepository: Repository<Event>,
 
-    @InjectRepository(User)
-    private userRepository: Repository<User>
+    @InjectRepository(Hashtag)
+    private hashtagRepo: Repository<Hashtag>
   ) {}
 
   async searchUser() {}
