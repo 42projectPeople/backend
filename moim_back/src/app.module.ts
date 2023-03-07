ee26ae2335150e90cddcffc875759c05f2be214d
myukang <myunghwan0421@gmail.com>
2023-03-07 18:31:51 +0900

    feat:remove drop schema

--- moim_back/src/app.module.ts
+++ moim_back/src/app.module.ts
@@ -33,7 +33,6 @@ import { SearchModule } from './search/search.module'
       entities: [Event, Hashtag, Review, User, User_Events],
       // NOTE: user_event의 drop 관련 문제 때문에 꺼뒀습니다.
       // https://github.com/42projectPeople/backend/discussions/39
-      dropSchema: true,
       synchronize: true, //특정 조건하에서 모든 데이터를 삭제하는 것 같습니다. 프로덕션에서는 사용하지 않는게 좋습니다.
       logging: true,
     }),
