e2ac54b7ec043fed72c551472dcf1c738f1a102e
myukang <myunghwan0421@gmail.com>
2023-03-21 20:24:55 +0900

    docs(api swagger): commit

--- moim_back/src/user/user.controller.ts
+++ moim_back/src/user/user.controller.ts
@@ -1,9 +1,9 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   DefaultValuePipe,
   Delete,
+  ForbiddenException,
   Get,
   HttpStatus,
   Param,
@@ -11,7 +11,9 @@ import {
   Post,
   Put,
   Query,
+  Req,
   Res,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common'
@@ -19,22 +21,24 @@ import { UserService } from './user.service'
 import { UpdateUserRequestDto } from './dto/updateUserRequestDto'
 import { CreateUserRequestDto } from './dto/createUserRequestDto'
 import { User } from '../entity/User.entity'
-import { EventData } from './utils/EventData'
-import {
-  ApiBody,
-  ApiOperation,
-  ApiParam,
-  ApiQuery,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger'
-import { Response } from 'express'
+import { ApiTags } from '@nestjs/swagger'
+import { Request, Response } from 'express'
 import { Users } from './utils/Users.type'
 import { RegisterEventRequestDto } from './dto/registerEventRequestDto'
 import { UnregisterEventRequestDto } from './dto/unregisterEventRequestDto'
 import { CheckNickNameResponseDto } from './dto/checkNickNameResponseDto'
-import { UserEventRoleType } from './utils/UserEventRoleType'
 import { UserRole } from './utils/UserRole.decorator'
+import { DocsCreateUser } from './swagger/DocsCreateUser.docs'
+import { DocscheckValidUserNickName } from './swagger/DocsCheckValidUserNickName.docs'
+import { DocsGetUserByUserId } from './swagger/DocsGetUserByUserId.docs'
+import { DocsGetUsersByPage } from './swagger/DocsGetUsersByPage.docs'
+import { DocsGetUserByNickName } from './swagger/DocsGetUserByNickName.docs'
+import { DocsUpdateUser } from './swagger/DocsUpdateUser.docs'
+import { DocsGetUserEvents } from './swagger/DocsGetUserEvents.docs'
+import { DocsRegisterEvent } from './swagger/DocsRegisterEvent.docs'
+import { DocsUnregisterEvent } from './swagger/DocsUnregisterEvent.docs'
+import { GetUserEventDto } from './dto/getUserEvents.dto'
+import { JWTAuthGuard } from 'src/auth/jwt-auth/jwt-auth.guard'
 
 @Controller('user')
 @ApiTags('user api')
@@ -60,19 +64,7 @@ export class UserController {
       forbidUnknownValues: true,
     })
   )
-  @ApiOperation({ summary: 'user creation api', description: 'create user' })
-  @ApiResponse({
-    status: HttpStatus.CREATED,
-    description: 'create user operation success',
-  })
-  @ApiResponse({
-    status: HttpStatus.CONFLICT,
-    description: 'create user operation is failed by conflict request',
-  })
-  @ApiBody({
-    type: CreateUserRequestDto,
-    description: 'Create user data',
-  })
+  @DocsCreateUser()
   async createUser(
     @Body()
     createUserDto: CreateUserRequestDto,
@@ -86,9 +78,12 @@ export class UserController {
    * check user name is already exists
    * @param userNickName
    */
-  @Post('verify/nickname/:userNickName')
-  async checkValidUserNickName(@Param('userNickName') userNickName: string) {
-    return
+  @Get('verify/nickname/:userNickName')
+  @DocscheckValidUserNickName()
+  async checkValidUserNickName(
+    @Param('userNickName') userNickName: string
+  ): Promise<CheckNickNameResponseDto> {
+    return { isValid: await this.userService.checkExistNickname(userNickName) }
   }
 
   /**
@@ -98,27 +93,7 @@ export class UserController {
    * @param res
    */
   @Get(':userID')
-  @ApiOperation({
-    summary: 'get user by user id',
-    description: 'get user by user id',
-  })
-  @ApiParam({
-    name: 'userID',
-    description: 'user id',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'User information',
-    type: User,
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'bad parameter',
-  })
-  @ApiResponse({
-    status: HttpStatus.NO_CONTENT,
-    description: 'there are no matched content',
-  })
+  @DocsGetUserByUserId()
   async getUserByUserId(
     @Param('userID', ParseIntPipe) userID: number,
     @Res({ passthrough: true }) res: Response
@@ -137,23 +112,7 @@ export class UserController {
   @Get()
   @UserRole('admin')
   // TODO: auth needed, auth checks user is admin user
-  @ApiOperation({
-    summary: 'get users by page',
-    description: 'get users by userID. 10 users returned by 1 page',
-  })
-  @ApiQuery({
-    name: 'page',
-    description: 'user list page',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: '10 User list by page',
-    type: Users,
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'bad query',
-  })
+  @DocsGetUsersByPage()
   async getUsersByPage(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number
   ): Promise<Users> {
@@ -166,23 +125,7 @@ export class UserController {
    * @param res
    */
   @Get('/nickname/:userNickName')
-  @ApiOperation({
-    summary: 'get user by nickname',
-    description: 'get user by nickname',
-  })
-  @ApiParam({
-    name: 'user nick name',
-    description: 'user nickname',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'get user by nickname',
-    type: User,
-  })
-  @ApiResponse({
-    status: HttpStatus.NO_CONTENT,
-    description: 'there are no user matched',
-  })
+  @DocsGetUserByNickName()
   async getUserByNickName(
     @Param('userNickName') userNickName: string,
     @Res({ passthrough: true }) res: Response
@@ -210,26 +153,7 @@ export class UserController {
     })
   )
   // TODO: auth needed
-  @ApiOperation({
-    summary: 'update user',
-    description: 'update user',
-  })
-  @ApiParam({
-    name: 'user id',
-    description: 'user id',
-  })
-  @ApiBody({
-    type: UpdateUserRequestDto,
-    description: 'data that user want to update',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'success update user',
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'invalid parameter',
-  })
+  @DocsUpdateUser()
   async updateUser(
     @Param('userID', ParseIntPipe) userID: number,
     @Body() updateUserDto: UpdateUserRequestDto
@@ -242,43 +166,60 @@ export class UserController {
    * @param userId
    * @param role
    */
-  @Get(':userID/event')
-  // TODO: auth needed
-  @ApiOperation({
-    summary: 'get user events',
-    description: 'get user register or hosted events.',
-  })
-  @ApiQuery({
-    name: 'role',
-    description: 'user role in event. (host || guest)',
-  })
-  @ApiParam({
-    name: 'userID',
-    description: 'user id',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    type: EventData,
-    description: 'user events',
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'bad request query',
-  })
-  async getUserEvents(
-    @Param('userID', ParseIntPipe) userId: number,
-    @Query('role') role: UserEventRoleType
-  ): Promise<EventData> {
-    if (role === UserEventRoleType.HOST) {
-      return {
-        events: await this.userService.findAllUserHostEvent(+userId),
-      }
-    } else if (role === undefined || role === UserEventRoleType.GUEST) {
-      return {
-        events: await this.userService.findAllUserGuestEvent(+userId),
-      }
-    } else {
-      throw new BadRequestException('invalid query')
+  @Get(':userId/event/host')
+  @UseGuards(JWTAuthGuard)
+  @DocsGetUserEvents()
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    })
+  )
+  async getUserEventsHost(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Query() userEventDto: GetUserEventDto,
+    @Req() req: Request
+  ) {
+    if (userId != req.user.userId)
+      throw new ForbiddenException('Forbidden resource')
+    return {
+      events: await this.userService.findAllUserHostEvent(
+        req.user.userId,
+        userEventDto
+      ),
+    }
+  }
+
+  /**
+   * RESTRICTED: login user
+   * @param userId
+   * @param role
+   */
+  @Get(':userId/event/guest')
+  @UseGuards(JWTAuthGuard)
+  @DocsGetUserEvents()
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    })
+  )
+  async getUserEventsGuest(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Query() userEventDto: GetUserEventDto,
+    @Req() req: Request
+  ) {
+    if (userId != req.user.userId)
+      throw new ForbiddenException('Forbidden resource')
+    return {
+      events: await this.userService.findAllUserGuestEvent(
+        req.user.userId,
+        userEventDto
+      ),
     }
   }
 
@@ -289,22 +230,7 @@ export class UserController {
    */
   @Post(':userID/event')
   // TODO: auth needed
-  @ApiOperation({
-    summary: 'register event',
-    description: 'register event',
-  })
-  @ApiBody({
-    type: RegisterEventRequestDto,
-    description: 'event id in json',
-  })
-  @ApiParam({
-    name: 'userID',
-    description: 'user id',
-  })
-  @ApiResponse({
-    status: HttpStatus.CREATED,
-    description: 'success register event',
-  })
+  @DocsRegisterEvent()
   @UsePipes(
     new ValidationPipe({
       transform: true,
@@ -326,22 +252,7 @@ export class UserController {
    * @param unregisterEventDto
    */
   @Delete(':userID/event')
-  @ApiOperation({
-    summary: 'unregister event',
-    description: 'unregister event',
-  })
-  @ApiBody({
-    type: UnregisterEventRequestDto,
-    description: 'event id, participation id in json',
-  })
-  @ApiParam({
-    name: 'userID',
-    description: 'user id',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'success unregister event',
-  })
+  @DocsUnregisterEvent()
   @UsePipes(
     new ValidationPipe({
       transform: true,
