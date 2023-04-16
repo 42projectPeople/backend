25bb92aeab211d159d82324423ca2d6815adde24
myukang <myunghwan0421@gmail.com>
2023-04-16 21:47:53 +0900

    feat(report module): commit

--- moim_back/src/report/report.module.ts
+++ moim_back/src/report/report.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ReportService } from './report.service'
 import { ReportController } from './report.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Report } from 'src/entity/Report.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Report])],
   controllers: [ReportController],
   providers: [ReportService],
 })
